1bdd2aface9458b464ac3d23eeb8bf9802ae25f8
MichaelWiss <michael.wiss@gmail.com>
2014-01-09 23:13:14 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt) {
+
+  // Configuration goes here
+  grunt.initConfig({
+  	 pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+          dist: {
+        		src: [
+        		'js/libs/*', //All JS in the libs folder
+        		'js/global.js'  //This specific file
+        		],
+        		dest: 'js/build/production.js',
+        	}
+    },
+
+       uglify: {
+    build: {
+        src: 'js/build/production.js',
+        dest: 'js/build/production.min.js'
+    }
+},
+
+     imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/'
+        }]
+    }
+},
+
+    watch: {
+       options: {
+        livereload: true,
+       },
+    scripts: {
+        files: ['js/*.js'],
+        tasks: ['jshint', 'concat', 'uglify'],
+        options: {
+            spawn: false,
+         }
+        },
+       css: {
+    files: ['css/*.scss'],
+    tasks: ['sass'],
+    options: {
+        spawn: false,
+    }
+    } 
+},
+
+    sass: {
+    dist: {
+        options: {
+            style: 'compressed'
+        },
+        files: {
+           'css/build/global.css': 'css/global.scss'
+
+        }
+    } 
+},
+      jshint: {
+    all: [  'js/global.js']
+  }
+
+
+
+  });
+
+  
+
+  // Load plugins here
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-imagemin');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-contrib-sass');
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Define your tasks here
+  
+  grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'sass', 'jshint']);
+  
+
+
+};

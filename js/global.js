2c58fcf4db265f4845b43d9804eba2680741872a
MichaelWiss <michael.wiss@gmail.com>
2014-01-15 19:41:00 -0500

    button styles

--- js/global.js
+++ js/global.js
@@ -1167,3 +1167,18 @@
 	var _skrollableIdCounter = 0;
 }(window, document));
 
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+

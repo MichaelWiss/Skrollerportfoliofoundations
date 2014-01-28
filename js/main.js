0f990257f8c28a997d8c92807a8999db1aae9be3
Michael Wiss <michael.wiss@gmail.com>
2014-01-28 01:07:49 -0500

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-<script>
-  skrollr.init({
+skrollr.init({
     forceHeight: false
   });
-  </script>
-
-  <script>
-  jQuery("#responsive_headline").fitText();
-</script>
-
-
-    <script>
+  
       $(document).foundation();
        threshold: 0
-    </script>
-
-    <script>
+    
   $(window).scroll(function() {
     $('#slideLeft').each(function(){
     var imagePos = $(this).offset().top;
@@ -25,9 +15,7 @@
       }
     });
   });
-</script>
 
-<script>
   $(window).scroll(function() {
     $('#slideRight').each(function(){
     var imagePos = $(this).offset().top;
@@ -38,4 +26,3 @@
       }
     });
   });
-</script>
